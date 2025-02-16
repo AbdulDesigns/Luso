f2dc46d380d27ed184eccc7375ddcb5b206ad624
abdul <abduldesigns24@gmail.com>
2025-02-16 16:47:10 +0530

    fix: enhance mobile menu functionality and styles for improved visibility and responsiveness

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ let navLists = document.querySelector(".navList");
 console.log("hello", menu, navLists);
 
 menu.addEventListener("click", () => {
-  navLists.style.transform = "translate(0,0)";
-  navLists.style.transition = "transform 0.5s ease-in";
+  //   navLists.style.transform = "translate(0,0)";
+  navLists.style.transition = "transform 0.5s ease";
   //   setTimeout(() => {
   //     navLists.style.transform = "translate(600px, 0)";
   //   }, 20000);
+
+  navLists.classList.toggle("menu-visible");
 });
