d1ae7b9a4b04eed59c16530e174591efaa4ce138
abdul <abduldesigns24@gmail.com>
2025-02-16 16:11:58 +0530

    fix: implement mobile navigation menu and enhance responsiveness

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 "use important";
 
-//search input logging
+//navigation menu toggling
 
+let menu = document.querySelector(".menu");
+let navLists = document.querySelector(".navList");
+
+console.log("hello", menu, navLists);
+
+menu.addEventListener("click", () => {
+  navLists.style.transform = "translate(0,0)";
+  navLists.style.transition = "transform 0.5s ease-in";
+  //   setTimeout(() => {
+  //     navLists.style.transform = "translate(600px, 0)";
+  //   }, 20000);
+});
